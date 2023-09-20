298077f24416f6f1b8d2091695907b90d37ba8bc
Andrei Lupu <alupu@fb.com>
2023-09-20 12:20:52 +0100

    make minatar overfit sweep

--- policy_distillation/sweep_overfit_minatar.py
+++ policy_distillation/sweep_overfit_minatar.py
@@ -27,18 +27,19 @@ executor.update_parameters(
 jobs = []
 with executor.batch():
     job_idx = 0
-    for D in [4, 16, 64]:
-        for env in ["SpaceInvaders-MinAtar", "Breakout-MinAtar", "Freeway-MinAtar", "Asterix-MinAtar"]:
-            for epochs in [50, 200, 400]:
+    for D in [32, 128]:
+        for env in ["SpaceInvaders-MinAtar"]:
+            for epochs in [16, 32, 64]:
                 for seed in [0]:#,1]:
-                    for sigma_init in [0.02]:#, 0.01]: #[0.02, 0.01, 0.005, 0.0025]:
-                        for sigma_decay in [1.0]:#, 0.999]: #, 0.995]:
-                            for lrate_init in [0.04, 0.01]: #[0.04, 0.02, 0.01, 0.005, 0.0025]: # Outer loop
-                                for lrate_decay in [1.0]:#, 0.999]: #, 0.995]: # Outer loop
-                                    for lr in [0.1, 0.05]: #[0.2, 0.1, 0.05, 0.025]: # Inner loop
-                                            for width in [64, 128]: #, 256]:
+                    for sigma_init in [1.5, 1.0, 0.5]:
+                        for sigma_decay in [1.0, 0.999]:
+                            for lrate_init in [0.1, 0.05]:  # Outer loop
+                                for lrate_decay in [1.0, 0.999]: # Outer loop
+                                    for lr in [0.01, 0.003, 0.001]: # Inner loop
+                                        for net in ["MLP"]:
+                                            for width in [512]: #, 256]:
                                                 print(env, D, epochs, seed)
-                                                folder = f"/private/home/alupu/explainable-policies/results/minatar/{env}/MLP/D{D}_learn_labels{int(False)}_norm{int(False)}/si{sigma_init}_sd{sigma_decay}_li{lrate_init}_ld{lrate_decay}/lr{lr}_E{epochs}_W{width}/seed{seed}_job_idx{job_idx}/"
+                                                folder = f"/private/home/alupu/explainable-policies/results/minatar_overfit/{env}/{net}_SNES/D{D}_learn_labels{int(False)}_norm{int(False)}/si{sigma_init}_sd{sigma_decay}_li{lrate_init}_ld{lrate_decay}/lr{lr}_E{epochs}_W{width}/seed{seed}_job_idx{job_idx}/"
                                                 if not os.path.exists(folder):
                                                     os.makedirs(folder)
                                                 argstring = \
@@ -53,13 +54,17 @@ with executor.batch():
                                                     f"--epochs {epochs} " \
                                                     f"--width {width} " \
                                                     f"--generations 1000 " \
-                                                    f"--popsize 1024 " \
-                                                    f"--rollouts 2 " \
-                                                    f"--eval_envs 4 " \
+                                                    f"--popsize 2048 " \
+                                                    f"--rollouts 1 " \
+                                                    f"--eval_envs 32 " \
                                                     f"--seed {seed} " \
                                                     f"--folder {folder} " \
+                                                    f"--overfit_seed 420 " \
+                                                    f"--const_normalize_obs 1 " \
+                                                    f"--net {net} " \
+                                                    f"--es_strategy SNES " \
                                                 
-#                                                 argstring = argstring + "--log_dataset "
+                                                argstring = argstring + "--overfit "
 
                                                 if not args.dry:
                                                     job = executor.submit(train_from_arg_string, argstring)
