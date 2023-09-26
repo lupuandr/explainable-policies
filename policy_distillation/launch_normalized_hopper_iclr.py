ea1cfe8893319ff75bd7ce607aa487eb3da9b479
Andrei Lupu <andrei.lupu@mail.mcgill.ca>
2023-09-26 17:55:07 +0000

    tweak slurm scripts

--- policy_distillation/launch_normalized_hopper_iclr.py
+++ policy_distillation/launch_normalized_hopper_iclr.py
@@ -1,5 +1,5 @@
 import submitit
-from distill_minatar import train_from_arg_string
+from distill_brax import train_from_arg_string
 import os
 import argparse
 
@@ -13,17 +13,17 @@ parser.add_argument(
 args = parser.parse_args()
 
 # Common args:
-root = "/private/home/alupu/explainable-policies/results/ICLR/minatar/virtual_batch_norm/"
+root = "/private/home/alupu/explainable-policies/results/ICLR/brax/"
 
-log_folder = f"/checkpoint/alupu/explainable_policies/minatar/submitit_logs/"
+log_folder = f"/checkpoint/alupu/explainable_policies/brax/submitit_logs/"
 if not os.path.exists(log_folder):
     os.makedirs(log_folder)
 
 
-def schedule_job(env, D, seed, epochs, folder, greedy_act, overfit, args):
+def schedule_job(env, D, seed, epochs, folder, overfit, args):
     if not os.path.exists(folder):
         os.makedirs(folder)
-
+        
     if overfit:
         rollouts = 1
     else:
@@ -39,11 +39,9 @@ def schedule_job(env, D, seed, epochs, folder, greedy_act, overfit, args):
         f"--rollouts {rollouts} " \
 
     argstring = argstring + "--normalize_obs 0 --const_normalize_obs 1 "
-
+    
     if overfit:
         argstring = argstring + f"--overfit --overfit_seed {420+seed} "
-    if greedy_act:
-        argstring = argstring + f"--greedy_act "
 
     if not args.dry:
         job = executor.submit(train_from_arg_string, argstring)
@@ -53,30 +51,33 @@ def schedule_job(env, D, seed, epochs, folder, greedy_act, overfit, args):
         return -1
 
 
+# SMALL ENVS
 executor = submitit.AutoExecutor(folder=log_folder)
 executor.update_parameters(
     slurm_partition="learnlab",
     gpus_per_node=8,
     cpus_per_task=80,
     timeout_min=300,
-    slurm_job_name="MinAtar",
+    slurm_job_name="hopper",
     slurm_exclude="learnfair2058"
 )
 
 jobs = []
 with executor.batch():
-    for env in ["SpaceInvaders-MinAtar", "Breakout-MinAtar", "Asterix-MinAtar", "Freeway-MinAtar"]:
-        for D in [16]:
+    for env in ["hopper"]:
+        for D in [4]:
             for seed in [0, 1, 2]:
-                for epochs in [32, 64]:
-                    for greedy_act in [True, False]:
-                        for overfit in [True, False]:
-                            if overfit:
-                                folder = f"{root}/overfit/{env}/D{D}/greedy_act{greedy_act}/seed{seed}/"
-                            else:
-                                folder = f"{root}/distill/{env}/D{D}/greedy_act{greedy_act}/seed{seed}/"
-                            job = schedule_job(env, D, seed, epochs, folder, greedy_act, overfit, args=args)
-                            jobs.append(job)
+                for overfit in [False]:
+
+                    epochs = 200 if env in ["hopper", "inverted_double_pendulum"] else 400
+
+                    if overfit:
+                        folder = f"{root}/overfit/{env}/D{D}/seed{seed}/"
+                        job = schedule_job(env, D, seed, epochs, folder, overfit=True, args=args)
+                    else:
+                        folder = f"{root}/distill/{env}/D{D}/seed{seed}/"
+                        job = schedule_job(env, D, seed, epochs, folder, overfit=False, args=args)
+                    jobs.append(job)
 
 if not args.dry:
     print(f"Launched {len(jobs)} jobs. May the gradients be ever in your favour!")
